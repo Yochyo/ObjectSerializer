8dbabf31c3dfa9fc8063c6e41209c2e81224a5f8
Philipp <herodragon25@gmail.com>
2019-12-05 21:36:12 +0100

    can now parse inheriting classes and field of any type

--- src/main/java/de/yochyo/objectserializer/ObjectSerializer.kt
+++ src/main/java/de/yochyo/objectserializer/ObjectSerializer.kt
@@ -4,13 +4,14 @@ import org.json.JSONObject
 import java.lang.reflect.Constructor
 import java.lang.reflect.Field
 
-//TODO @Ignore Annotation hinzufügen
 fun main() {
-    val d = Dummy("testvalue", 5, 5.0)
+    val d = Dummy("testvalue")
+    d.a = "test_if_it_works"
+    d.field.field2 = "field22"
+    d.superParent = "super"
     val json = ObjectSerializer.toJsonObject(d)
     println(json)
-    val o = ObjectSerializer.toObject(json, Dummy::class.java)
-    println(o)
+    println(ObjectSerializer.toObject(json, Dummy::class.java))
 }
 
 object ObjectSerializer {
@@ -21,11 +22,17 @@ object ObjectSerializer {
      */
     fun toJsonObject(o: Any): JSONObject {
         val json = JSONObject()
-        for (field in o.javaClass.declaredFields)
+        classToJson(o, o.javaClass, json)
+        return json
+    }
+
+    private fun classToJson(o: Any, clazz: Class<*>, json: JSONObject): JSONObject {
+        for (field in clazz.declaredFields)
             accessField(field) {
                 if (!field.isAnnotationPresent(Ignore::class.java))
-                    json.put(it.name, it.get(o))
+                    addFieldToJson(o, field, json)
             }
+        if (hasValidSuperclass(clazz)) json.put("super", classToJson(o, clazz.superclass, JSONObject()))
         return json
     }
 
@@ -40,15 +47,35 @@ object ObjectSerializer {
     fun <E> toObject(json: JSONObject, clazz: Class<E>): E {
         val defaultConstructor = getDefaultConstructor<E>(clazz.constructors)
         val o = defaultConstructor.newInstance()
+        jsonToClass(o, clazz, json)
+        return o
+    }
 
+    private fun <E> jsonToClass(o: E, clazz: Class<*>, json: JSONObject): E {
         for (field in clazz.declaredFields) {
             accessField(field) {
                 if (!field.isAnnotationPresent(Ignore::class.java))
-                    it.set(o, json[it.name] ?: throw Exception("Json does not contain field \"${field.name}\""))
+                    jsonToField(o as Any, field, json)
             }
         }
+        if (hasValidSuperclass(clazz)) jsonToClass(o, clazz.superclass, json["super"] as JSONObject)
         return o
     }
+
+    private fun addFieldToJson(o: Any, field: Field, json: JSONObject) {
+        accessField(field) {
+            if (isPrimitiveOrString(field)) json.put(field.name, field.get(o))
+            else json.put(field.name, classToJson(field.get(o), field.type, JSONObject()))
+        }
+    }
+
+    private fun jsonToField(o: Any, field: Field, json: JSONObject) {
+        accessField(field) {
+            if (field.type.isPrimitive || field.type == String().javaClass) field.set(o, json[field.name])
+            else field.set(o, jsonToClass(field.get(o), field.type, json[field.name] as JSONObject))
+        }
+    }
+
 
     private fun <E> getDefaultConstructor(constructors: Array<Constructor<*>>): Constructor<E> {
         for (constructor in constructors)
@@ -63,33 +90,59 @@ object ObjectSerializer {
         field.isAccessible = accessible
     }
 
+    private fun hasValidSuperclass(clazz: Class<*>): Boolean {
+        val superclass = clazz.superclass
+        return superclass != null && superclass != Object().javaClass
+    }
+    private fun isPrimitiveOrString(field: Field) = field.type.isPrimitive || field.type == String().javaClass
 }
 
 class Dummy : Parent {
-    protected val int: Int
-    val double: Double
-    private val test: String
+    val field = FieldInClass()
 
-    @Ignore
     private val initialized = "init"
 
-    constructor() : this("", 0, 0.0)
-    constructor(test: String, int: Int, double: Double) : super("parent") {
-        this.test = test
-        this.int = int
-        this.double = double
+    constructor() : this("")
+    constructor(test: String) : super("parent") {
     }
 
     override fun toString(): String {
-        return test + " " + initialized + " " + int + " " + double
+        return "${super.toString()} $field $initialized"
     }
 }
 
-open class Parent {
-    val a: String
+class FieldInClass {
+    var field1 = "field1"
+    var field2 = "field2"
+
+    override fun toString(): String {
+        return "field1=$field1 field2=$field2"
+    }
+}
+
+open class Parent : SuperParent {
+    var a: String
+    private val test: String = "test_in_parent"
 
     constructor() : this("")
     constructor(a: String) {
         this.a = a
+    }
+
+    override fun toString(): String {
+        return super.toString() + "a=$a"
+    }
+}
+
+open class SuperParent {
+    var superParent: String
+
+    constructor() : this("")
+    constructor(a: String) {
+        this.superParent = a
+    }
+
+    override fun toString(): String {
+        return "super($superParent)"
     }
 }
