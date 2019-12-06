a8a94fa375c804630586030be712e03b6bd96770
Philipp <herodragon25@gmail.com>
2019-12-06 17:51:43 +0100

    you can now safe arrays

--- src/main/java/de/yochyo/objectserializer/ObjectSerializer.kt
+++ src/main/java/de/yochyo/objectserializer/ObjectSerializer.kt
@@ -1,15 +1,16 @@
 package de.yochyo.objectserializer
 
+import org.json.JSONArray
 import org.json.JSONObject
 import java.lang.reflect.Constructor
 import java.lang.reflect.Field
 
+//TODO Array of Objects
+//TODO Can not set static final long field java.util.ArrayList.serialVersionUID to java.lang.Long
+//TODO implement lambdas
 fun main() {
     val d = Dummy("testvalue")
-    d.a = "test_if_it_works"
-    d.field.field2 = "field22"
-    d.superParent = "super"
-    val json = ObjectSerializer.toJsonObject(d)
+    val json = ObjectSerializer.toJSONObject(d)
     println(json)
     println(ObjectSerializer.toObject(json, Dummy::class.java))
 }
@@ -20,19 +21,9 @@ object ObjectSerializer {
      * @param o Any not-null object
      * @return JSONObject containing all fields of o
      */
-    fun toJsonObject(o: Any): JSONObject {
+    fun toJSONObject(o: Any): JSONObject {
         val json = JSONObject()
         classToJson(o, o.javaClass, json)
-        return json
-    }
-
-    private fun classToJson(o: Any, clazz: Class<*>, json: JSONObject): JSONObject {
-        for (field in clazz.declaredFields)
-            accessField(field) {
-                if (!field.isAnnotationPresent(Ignore::class.java))
-                    addFieldToJson(o, field, json)
-            }
-        if (hasValidSuperclass(clazz)) json.put("super", classToJson(o, clazz.superclass, JSONObject()))
         return json
     }
 
@@ -45,42 +36,67 @@ object ObjectSerializer {
      * @throws Exception
      */
     fun <E> toObject(json: JSONObject, clazz: Class<E>): E {
-        val defaultConstructor = getDefaultConstructor<E>(clazz.constructors)
+        val defaultConstructor = getDefaultConstructor(clazz.constructors as Array<Constructor<E>>)
         val o = defaultConstructor.newInstance()
         jsonToClass(o, clazz, json)
         return o
+    }
+
+    private fun classToJson(o: Any, clazz: Class<*>, json: JSONObject): JSONObject {
+        for (field in clazz.declaredFields)
+            accessField(field) {
+                if (!field.isAnnotationPresent(Ignore::class.java))
+                    readField(o, field, json)
+            }
+
+        if (hasValidSuperclass(clazz)) json.put("super", classToJson(o, clazz.superclass, JSONObject()))
+        return json
     }
 
     private fun <E> jsonToClass(o: E, clazz: Class<*>, json: JSONObject): E {
         for (field in clazz.declaredFields) {
             accessField(field) {
                 if (!field.isAnnotationPresent(Ignore::class.java))
-                    jsonToField(o as Any, field, json)
+                    writeField(o as Any, field, json)
             }
         }
         if (hasValidSuperclass(clazz)) jsonToClass(o, clazz.superclass, json["super"] as JSONObject)
         return o
     }
 
-    private fun addFieldToJson(o: Any, field: Field, json: JSONObject) {
+    private fun readField(o: Any, field: Field, json: JSONObject) {
         accessField(field) {
             if (isPrimitiveOrString(field)) json.put(field.name, field.get(o))
+            else if (field.type.isArray) json.put(field.name, readArray(o, field))
             else json.put(field.name, classToJson(field.get(o), field.type, JSONObject()))
         }
     }
 
-    private fun jsonToField(o: Any, field: Field, json: JSONObject) {
+    private fun writeField(o: Any, field: Field, json: JSONObject) {
         accessField(field) {
-            if (field.type.isPrimitive || field.type == String().javaClass) field.set(o, json[field.name])
+            if (isPrimitiveOrString(field)) field.set(o, json[field.name])
+            else if (field.type.isArray) field.set(o, readJSONArray(json[field.name] as JSONArray))
             else field.set(o, jsonToClass(field.get(o), field.type, json[field.name] as JSONObject))
         }
     }
 
+    private fun readJSONArray(json: JSONArray): Array<Any> {
+        val clazz = Class.forName(json[0].toString())
+        val array = java.lang.reflect.Array.newInstance(clazz, json.length() - 1) as Array<Any>
 
-    private fun <E> getDefaultConstructor(constructors: Array<Constructor<*>>): Constructor<E> {
-        for (constructor in constructors)
-            if (constructor.parameters.isEmpty()) return constructor as Constructor<E>
-        throw Exception("Class does not contain a default constructor")
+        if (isPrimitveOrStringArray(array)) for (i in 0 until json.length() - 1) array[i] = json[i + 1]
+        else for (i in 0 until json.length() - 1) array[i] = toObject(json[i + 1] as JSONObject, clazz)
+        return array
+    }
+
+    private fun readArray(o: Any, field: Field): JSONArray {
+        val json = JSONArray()
+        val array = field.get(o) as Array<Any>
+        json.put(array.javaClass.name.substring(2, array.javaClass.name.length - 1))
+
+        if (isPrimitveOrStringArray(array)) array.forEach { json.put(it) }
+        else array.forEach { json.put(toJSONObject(it)) }
+        return json
     }
 
     private fun accessField(field: Field, run: (field: Field) -> Unit) {
@@ -90,59 +106,40 @@ object ObjectSerializer {
         field.isAccessible = accessible
     }
 
+    private fun <E> getDefaultConstructor(constructors: Array<Constructor<E>>): Constructor<E> {
+        for (constructor in constructors)
+            if (constructor.parameters.isEmpty()) return constructor
+        throw Exception("Class does not contain a default constructor")
+    }
+
     private fun hasValidSuperclass(clazz: Class<*>): Boolean {
         val superclass = clazz.superclass
         return superclass != null && superclass != Object().javaClass
     }
+
+    private fun isPrimitveOrStringArray(array: Array<Any>) = Array<String>::class.java.isAssignableFrom(array.javaClass) || Array<java.lang.Number>::class.java.isAssignableFrom(array.javaClass)
     private fun isPrimitiveOrString(field: Field) = field.type.isPrimitive || field.type == String().javaClass
 }
 
-class Dummy : Parent {
-    val field = FieldInClass()
-
+class Dummy {
+    // val lambda: () -> Unit = { println("lambda_testssss") }
+    val array = emptyArray<Int>()
+    val fooArray = arrayOf(Foo(), Foo())
     private val initialized = "init"
 
     constructor() : this("")
-    constructor(test: String) : super("parent") {
+    constructor(test: String) {
+        // val a = lambda.javaClass
     }
 
     override fun toString(): String {
-        return "${super.toString()} $field $initialized"
+        return "$initialized [${array.joinToString { it.toString() }}]  [${fooArray.joinToString { it.toString() }}]"
     }
 }
 
-class FieldInClass {
-    var field1 = "field1"
-    var field2 = "field2"
-
+class Foo {
+    val foo = "1"
     override fun toString(): String {
-        return "field1=$field1 field2=$field2"
-    }
-}
-
-open class Parent : SuperParent {
-    var a: String
-    private val test: String = "test_in_parent"
-
-    constructor() : this("")
-    constructor(a: String) {
-        this.a = a
-    }
-
-    override fun toString(): String {
-        return super.toString() + "a=$a"
-    }
-}
-
-open class SuperParent {
-    var superParent: String
-
-    constructor() : this("")
-    constructor(a: String) {
-        this.superParent = a
-    }
-
-    override fun toString(): String {
-        return "super($superParent)"
+        return foo
     }
 }
