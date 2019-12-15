6c86da22c1a2ed98bf8a01c2288e2b570439c108
Yochyo <herodragon25@gmail.com>
2019-12-15 21:08:58 +0100

    Some new functions

--- src/main/java/de/yochyo/objectserializer/ObjectSerializer.kt
+++ src/main/java/de/yochyo/objectserializer/ObjectSerializer.kt
@@ -2,6 +2,7 @@ package de.yochyo.objectserializer
 
 import de.yochyo.objectserializer.annotations.Serializeable
 import de.yochyo.objectserializer.parser.Parser
+import org.json.JSONArray
 import org.json.JSONObject
 
 object ObjectSerializer {
@@ -12,7 +13,13 @@ object ObjectSerializer {
      * @param o Any not-null object
      * @return JSONObject containing all fields of o
      */
-    fun toJSONObject(o: Any): JSONObject = Parser.toJSON(o, o.javaClass) as JSONObject
+    fun toJSON(o: Any) = Parser.toJSON(o, o.javaClass)
+
+    fun objectToJson(o: Any) = toJSON(o) as JSONObject
+    fun serializeableToString(serializeable: Any) = Parser.toJSON(serializeable, serializeable.javaClass, arrayOf(Serializeable::class.java.name)) as String
+    fun arrayToJson(array: Array<*>) = toJSON(array) as JSONArray
+    fun collectionToJson(collection: Collection<*>) = toJSON(collection) as JSONArray
+
 
     /**
      * Takes a JSONObject and will parse it to a class of type E. This will only work if the class,
@@ -26,7 +33,5 @@ object ObjectSerializer {
      */
     fun <E> toObject(json: JSONObject, clazz: Class<E>) = Parser.toObject(json, clazz)
 
-    private val serializeableArray = arrayOf(Serializeable::class.java.name)
-    fun serializeableToJSONObject(o: Any) = Parser.toJSON(o, o.javaClass, serializeableArray)
-    fun <E> serializeableToObject(o: Any, clazz: Class<E>) = Parser.toObject(o, clazz, serializeableArray)
+    fun <E> serializeableToObject(serializeable: String, clazz: Class<E>) = Parser.toObject(serializeable, clazz, arrayOf(Serializeable::class.java.name))
 }
